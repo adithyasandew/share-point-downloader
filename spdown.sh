35c9d98d3d8686b581f8d8a24814d176aa15c10c
Adithya Sandew <adithyasrithikasandiw@gmail.com>
2024-03-22 03:54:47 +0530

    add download

--- spdown.sh
+++ spdown.sh
@@ -6,14 +6,32 @@ generate_filename() {
 }
 
 echo "Enter video manifest link :"
-read link
+read vlink
+
+echo "Enter video name to save :"
+read save_name
+
 
 new_filename=$(generate_filename)
 
-curl "$link" -o "$new_filename"
+curl "$vlink" -o "$new_filename"
 
 if [ $? -eq 0 ]; then
     echo "[+] Video manifest file downloaded"
 else
     echo "[-] Failed to download video manifest file."
+fi
+
+
+if [ -f "$new_filename" ]; then
+
+    ffmpeg -i "$new_filename" -codec copy "$save_name"
+
+    if [ $? -eq 0 ]; then
+        echo "[+] Video downloaded successfully and saved as: $save_name"
+    else
+        echo "[-] Error: Failed to download video."
+    fi
+else
+    echo "[-] Error: File '$filename' not found."
 fi
