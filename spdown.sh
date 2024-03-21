6f7591b0855b02dbab1aac8f15b7c3cbb58e60f5
Adithya Sandew <adithyasrithikasandiw@gmail.com>
2024-03-22 03:19:26 +0530

    fix name conflict

--- spdown.sh
+++ spdown.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+generate_filename() {
+    local filename
+    filename="response_$(date +%s%N).txt"
+    echo "$filename"
+}
 
-echo "Enter video manifest link:"
+echo "Enter video manifest link :"
 read link
 
-curl "$link" -o video_manifest.txt
+new_filename=$(generate_filename)
+
+curl "$link" -o "$new_filename"
 
 if [ $? -eq 0 ]; then
     echo "[+] Video manifest file downloaded"
